216314a165922ab132196c05af27ca69746c0040
IacheLopes <IacheLopes@users.noreply.github.com>
2026-04-23 19:57:37 -0300

    Offline: página em branco até regularização de pagamento
    Para restaurar: git checkout backup/site-completo -- src/App.jsx && git commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,5 @@
-import Navbar from './components/Navbar/Navbar';
-import Hero from './components/Hero/Hero';
-import Features from './components/Features/Features';
-import Benefits from './components/Benefits/Benefits';
-import FAQ from './components/FAQ/FAQ';
-import Contact from './components/Contact/Contact';
-import Footer from './components/Footer/Footer';
-import WhatsAppButton from './components/WhatsAppButton/WhatsAppButton';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <main>
-        <Hero />
-        <Features />
-        <Benefits />
-        <FAQ />
-        <Contact />
-      </main>
-      <Footer />
-      <WhatsAppButton />
-    </div>
-  );
+  return null;
 }
 
 export default App;
